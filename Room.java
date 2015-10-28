3522acbccdad99ca442089e63c8179986644b03a
daveyjones94 <d@vidm.co>
2015-10-27 23:55:52 -0400

    Modifications to Room in Preparation
    This class now contains:
    -Methods to handle new data types like NPCs and Items
    -Constructors to handle new definitions of room
    -Making room responsible for more data

--- Room.java
+++ Room.java
@@ -19,19 +19,65 @@ import java.util.Iterator;
 public class Room 
 {
     private String description;
+    private String narrativeDescription;
+    //private NonplayableChar npc;
+    //private Item item;
     private HashMap<String, Room> exits;        // stores exits of this room.
 
     /**
      * Create a room described "description". Initially, it has
      * no exits. "description" is something like "a kitchen" or
      * "an open court yard".
-     * @param description The room's description.
+     * A narrative description describes what Van Helsing sees
+     * when using rhe look command inside a room or location.
+     * @param description          The room's description.
      */
     public Room(String description) 
     {
         this.description = description;
+        //narrativeDescription = null;
+        //npc = null;
+        //item = null;
         exits = new HashMap<String, Room>();
     }
+    
+    /**
+     * This is a constructor for a location which will also be
+     * passed a more detailed description of the scenery given
+     * elements of the location itself along with written text
+     * for the imagery of Van Helsing's thoughts.
+     * @param description           The location's description.
+     * @param narrativeDescription  A detailed view of the location from Van Helsing's perspective.
+     */
+    public Room(String description, String narrativeDescription)
+    {
+        this.description = description;
+        this.narrativeDescription = narrativeDescription;
+        //npc = null;
+        //item = null;
+    }
+    
+    /**
+     * THIS METHOD HAS NO IMPLEMENTATION AT THIS POINT IN TIME
+     * This method generates a room with a non-playable
+     * character such as a shopkeep and items to go with the
+     * location that are not part of the NPC's inventory. If
+     * no item is in the room by itself then it can just be
+     * set to null.
+     * @param description           The location's description.
+     * @param narrativeDescription  A detailed view of the location from Van Helsing's perspective.
+     * @param npc                   A non-playable character in this room.
+     * @param item                  An item that exists inside the room that isn't already part
+     *                              of the NPC's inventory.
+     */
+    /*
+     * public Room(String description, String narrativeDescription, NonplayableChar npc, Item item){
+     *     this.description = description;
+     *     this.narrativeDescription = narrativeDescription;
+     *     this.npc = npc;
+     *     this.item = item;
+     * }
+     */
 
     /**
      * Define an exit from this room.
@@ -42,7 +88,56 @@ public class Room
     {
         exits.put(direction, neighbor);
     }
+    
+    /**
+     * THIS METHOD HAS NO IMPLEMENTATION AT THIS POINT IN TIME
+     * Return the item present in the room if there is one.
+     * @param Item   The item of the room.
+     */
+    /*
+     * public void setItem(Item item)
+     * {
+     *     this.item = item;
+     * }
+     */
+    
+    /**
+     * THIS METHOD HAS NO IMPLEMENTATION AT THIS POINT IN TIME
+     * Define an NPC from the room if there is one.
+     * @param npc   The NPC of the room.
+     */
+    /*
+     * public void setNonplayableChar(NonplayableChar npc)
+     * {
+     *     this.npc = npc;
+     * }
+     */
+    
+    /**
+     * THIS METHOD HAS NO IMPLEMENTATION AT THIS POINT IN TIME
+     * Return the item present in the room if there is one.
+     * @return   The item of the room.
+     */
+    /*
+     * public Item getItem()
+     * {
+     *     return item;
+     * }
+     */
 
+    /**
+     * THIS METHOD HAS NO IMPLEMENTATION AT THIS POINT IN TIME
+     * Return the NPC present in the room if there is one.
+     * @return   The NPC of the room.
+     */
+    /*
+     * public NonplayableChar getNonplayableChar()
+     * {
+     *     return npc;
+     * }
+     */
+
+    
     /**
      * @return The short description of the room
      * (the one that was defined in the constructor).
@@ -62,7 +157,28 @@ public class Room
     {
         return "You are " + description + ".\n" + getExitString();
     }
-
+    
+    /**
+     * THIS METHOD HAS NO COMPLETE IMPLEMENTATION AT THIS POINT IN TIME
+     * Return a narrative description from the perspective of
+     * Van Helsing along with a description of how many NPCs
+     * are in the room and whether there are any items in
+     * plain sight.
+     * @return Detailed elements of the room, including NPCs and items.
+     */
+    
+    private String getNarrativeDescription()
+    {
+        String returnString = narrativeDescription;
+        /*if (npc != null) {
+            returnString += "\nHere with Van Helsing is " + npc.getName();
+        }
+        if (item != null) {
+            returnString += "\nAbout the location, Van Helsing sees something: " + item.getName();
+        }*/
+        return returnString;
+    }
+    
     /**
      * Return a string describing the room's exits, for example
      * "Exits: north west".
